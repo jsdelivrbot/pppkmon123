e085d3db3729d7ff11239f38b5dd4527b4bae416
Nikokatsu <sharmadipesh224@gmail.com>
2018-01-04 18:14:02 -0800

    Update chat-commands.js

--- chat-commands.js
+++ chat-commands.js
@@ -1520,7 +1520,7 @@ exports.commands = {
 		if (target.startsWith('http://')) target = target.slice(7);
 		if (target.startsWith('https://')) target = target.slice(8);
 		if (target.startsWith('play.pokemonshowdown.com/')) target = target.slice(25);
-		if (target.startsWith('psim.us/')) target = target.slice(8);
+		if (target.startsWith('.com/')) target = target.slice(8);
 		if (user.tryJoinRoom(target, connection) === Rooms.RETRY_AFTER_LOGIN) {
 			connection.sendTo(target, "|noinit|namerequired|The room '" + target + "' does not exist or requires a login to join.");
 		}
